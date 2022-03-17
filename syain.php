aa30617b2edfc88fadef442b5ec897cb80f3e528
sworc <lightbox@nifty.com>
2022-03-17 11:25:51 +0900

    Add files via upload

--- syain.php
+++ syain.php
@@ -39,6 +39,24 @@ if ( $_POST["btn"] == "確認" ) {
         $_POST["kanri"] = $row["管理者"];
         $_POST["kanri_name"] = $row["管理者名"];
         $_POST["birth"] = $row["生年月日"];
+
+        $syozoku_option = "";
+        $query = <<<QUERY
+        select * from コード名称マスタ where 区分 = 2
+        QUERY;
+        
+        $result = $mysqli->query( $query );
+        while ( $row = $result->fetch_array( MYSQLI_BOTH ) ) {
+            if ( $_POST["syozoku"] == $row["コード"] ) {
+                $syozoku_option .= 
+                    "<option selected value='{$row["コード"]}'>{$row["名称"]}</option>";
+            }
+            else {
+                $syozoku_option .= 
+                    "<option value='{$row[0]}'>{$row["名称"]}</option>";
+            }
+        }
+        
     }
     else {
         $_POST["sname"] = "";
